90c4a39720d0453bac62bc20fc700298413a422c
xieck13 <xieck13@gmail.com>
2024-10-23 23:05:02 +0800

    add redis

--- image-downloader/start_redis.sh
+++ image-downloader/start_redis.sh
@@ -1,2 +1,5 @@
 # start redis server
-redis-server --requirepass "****123***"
+apt update && apt install -y redis
+
+# 启动redis-server在daemon模式
+redis-server --requirepass "****123***" --daemonize yes
