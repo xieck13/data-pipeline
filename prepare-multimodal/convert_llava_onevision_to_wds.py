403063ed275809b57093135c6b2e537ff1c9139b
xieck13 <xieck13@gmail.com>
2024-10-28 12:11:39 +0800

    fix bug

--- prepare-multimodal/convert_llava_onevision_to_wds.py
+++ prepare-multimodal/convert_llava_onevision_to_wds.py
@@ -20,7 +20,7 @@ parquet_paths = glob.glob(os.path.join(llava_pretrain_dir, "*", "*.parquet"))
 output = os.path.join(output_llava_pretrain_dir, 'train')
 os.makedirs(output, exist_ok=True)
 error_path = defaultdict(int)
-
+count = 0
 
 with wds.ShardWriter(os.path.join(output, 'pretrain-%d.tar'), maxcount=10000) as shard_writer:
     for path in tqdm(parquet_paths):
@@ -33,7 +33,7 @@ with wds.ShardWriter(os.path.join(output, 'pretrain-%d.tar'), maxcount=10000) as
                     context = conv[conv_id]['value']
                     answers = conv[conv_id + 1]['value']
                     image = row['image']['bytes']
-                    key = row['id'].replace('/', '_')
+                    key = row['id'].replace('.', '_').replace('/', '_')
                     sample = {
                         "__key__": key + f"_{conv_id}",
                         "image": image,
@@ -41,8 +41,12 @@ with wds.ShardWriter(os.path.join(output, 'pretrain-%d.tar'), maxcount=10000) as
                         "answers": answers,
                     }
                     shard_writer.write(sample)
+                    count += 1
+                    if count % 10000 == 0:
+                        print(f"Processed {count} samples")
                 except Exception as e:
                     error_path[path] += 1
                     traceback.print_exc()
 
+print(f"Total samples: {count}")
 print(f"Dataset successfully converted to wds")
