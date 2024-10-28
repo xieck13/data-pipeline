d1c5a992d5022ac8b31068b2c181e95e7abb7783
xieck13 <xieck13@gmail.com>
2024-10-28 14:07:46 +0800

    update

--- prepare-multimodal/convert_llava_onevision_to_wds.py
+++ prepare-multimodal/convert_llava_onevision_to_wds.py
@@ -10,8 +10,10 @@ from collections import defaultdict
 
 import traceback
 
-llava_pretrain_dir = './onevision'
-output_llava_pretrain_dir = './onevision_wds'
+import json
+
+llava_pretrain_dir = '/volume/siflow/pt_data/ovdata/'
+output_llava_pretrain_dir = '/volume/siflow/pt_data/ovdata_wds'
 
 os.makedirs(output_llava_pretrain_dir, exist_ok=True)
 
@@ -19,12 +21,12 @@ parquet_paths = glob.glob(os.path.join(llava_pretrain_dir, "*", "*.parquet"))
 # Paths to the dataset files
 output = os.path.join(output_llava_pretrain_dir, 'train')
 os.makedirs(output, exist_ok=True)
-error_path = defaultdict(int)
+error_path_counter = defaultdict(int)
+error_samples = []
 count = 0
 
 with wds.ShardWriter(os.path.join(output, 'pretrain-%d.tar'), maxcount=10000) as shard_writer:
     for path in tqdm(parquet_paths):
-        print(error_path[path])
         df = pd.read_parquet(path)
         for i, row in df.iterrows():
             conv = row['conversations'].tolist()
@@ -44,9 +46,21 @@ with wds.ShardWriter(os.path.join(output, 'pretrain-%d.tar'), maxcount=10000) as
                     count += 1
                     if count % 10000 == 0:
                         print(f"Processed {count} samples")
+                        print(f"Error paths: {error_path_counter}")
                 except Exception as e:
-                    error_path[path] += 1
+                    error_path_counter[path] += 1
+                    error_samples.append(
+                        {
+                            "path": path,
+                            "row_id": i,
+                            "conv_id": conv_id,
+                            "error": str(e),
+                        }
+                    )
                     traceback.print_exc()
 
 print(f"Total samples: {count}")
+print(f"Error paths: {error_path_counter}")
+print(f"Error samples: {error_samples}")
+json.dump(error_samples, open(os.path.join(output_llava_pretrain_dir, 'error_samples.json'), 'w'))
 print(f"Dataset successfully converted to wds")
