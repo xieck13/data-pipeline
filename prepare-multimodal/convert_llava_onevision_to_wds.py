6774208d95b28aacca436827ebd44bd14e3a0a47
xieck13 <xieck13@gmail.com>
2024-10-28 12:10:21 +0800

    fix bug

--- prepare-multimodal/convert_llava_onevision_to_wds.py
+++ prepare-multimodal/convert_llava_onevision_to_wds.py
@@ -10,14 +10,14 @@ from collections import defaultdict
 
 import traceback
 
-llava_pretrain_dir = '/gpfs/huggingface'
-output_llava_pretrain_dir = '/gpfs/huggingface'
+llava_pretrain_dir = './onevision'
+output_llava_pretrain_dir = './onevision_wds'
 
 os.makedirs(output_llava_pretrain_dir, exist_ok=True)
 
 parquet_paths = glob.glob(os.path.join(llava_pretrain_dir, "*", "*.parquet"))
 # Paths to the dataset files
-output = os.path.join(output_llava_pretrain_dir, 'wds_test')
+output = os.path.join(output_llava_pretrain_dir, 'train')
 os.makedirs(output, exist_ok=True)
 error_path = defaultdict(int)
 
@@ -31,13 +31,14 @@ with wds.ShardWriter(os.path.join(output, 'pretrain-%d.tar'), maxcount=10000) as
             for conv_id in range(0, len(conv), 2):
                 try:
                     context = conv[conv_id]['value']
-                    answer = conv[conv_id + 1]['value']
-                    image = row['image']['bytes']       
+                    answers = conv[conv_id + 1]['value']
+                    image = row['image']['bytes']
+                    key = row['id'].replace('/', '_')
                     sample = {
-                        "__key__": row['id'] + f"_{conv_id}",
+                        "__key__": key + f"_{conv_id}",
                         "image": image,
                         "context": context,
-                        "answer": answer,
+                        "answers": answers,
                     }
                     shard_writer.write(sample)
                 except Exception as e:
